6efbe2ada289680c736149a0c5ac92b6f313e36c
Santtu Lintervo <santervo@gmail.com>
2011-04-04 20:52:51 +0300

    change version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rsmapper "0.0.2"
+(defproject rsmapper "0.1.0-SNAPSHOT"
   :description "SQL result set mapping library."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
