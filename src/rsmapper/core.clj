8261a8b612d6270f749a863a4c7e6cb061ab2e47
Santtu Lintervo <santervo@gmail.com>
2011-04-26 22:56:57 +0300

    renamed functions

--- src/rsmapper/core.clj
+++ src/rsmapper/core.clj
@@ -1,28 +1,29 @@
 (ns rsmapper.core)
 
-(defn- row-map-as [k fields row]
-  (let [dependent (select-keys row fields)
-        row (apply dissoc row fields)]
-    (assoc row k dependent)))
+(defn- nest-row [row k cols]
+  (let [nested (select-keys row cols)
+        row (apply dissoc row cols)]
+    (assoc row k nested)))
 
-(defn map-as [result-set k fields]
-  "Map fields in each row of result-set as nested map with key k."
-  (map (partial row-map-as k fields) result-set))
+(defn nest [result-set k cols]
+  "Map columns in each row of result-set as nested map with key k."
+  (map (partial row-map-as k cols) result-set))
 
-(defn collect-as [result-set k ks]
+(defn collect [result-set k ks]
   "Groups rows from result-set by all fields excluding field ks, and collects all ks fields
   as collection keyd k"
   (let [grouping-f (fn [row] (dissoc row ks))
         combiner-f (fn [[row coll-rows]] (assoc row k (map ks coll-rows)))]
     (map combiner-f (group-by grouping-f result-set))))
 
-(defn map-as-collection [result-set k fields]
-  "Maps fields in each row of result-set as nested map and collects them with key k."
-  (-> result-set (map-as k fields) (collect-as k k)))
-
-(defn include-as [result-set k other id other-id]
-  "Groups rows in other by field id in result-set and other-id in other. Associates collections
-  with key k to result-set rows."
-  (let [collections (group-by other-id other)]
-    (map #(assoc % k (collections (% id))) result-set)))
+(defn join [rs k other-rs matching-f grouping-f]
+  "Joins rs with other-rs by creating a new map for every row r1 in rs
+  and row r2 in other-rs where (= (matching-f r1) (grouping-f r2)) so that
+  r2 is associated with key k to r1. If no row in other-rs matches a row r in rs,
+  then nil is associated with key k to r. Keywords make excellent candidates
+  for matching-f and grouping-f."
+  (let [collections (group-by grouping-f other-rs)]
+    (for [row rs
+          other-row (or (collections (matching-f row) [nil]))]
+      (assoc row k other-row))))
 
