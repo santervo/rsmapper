86e4376d177a9dca1a5e2f4c78d77386b86119e9
Santtu Lintervo <santervo@gmail.com>
2011-04-28 21:20:28 +0300

    add lein-vimclojure

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject rsmapper "0.1.0-SNAPSHOT"
   :description "SQL result set mapping library."
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]])
